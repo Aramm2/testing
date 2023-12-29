2ce0b304cd82b3938c7033a56ef35bdf19283544
Aram Satouf <66645780+Aramm2@users.noreply.github.com>
2023-12-29 23:39:58 +0400

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
+# If you want to set execute permissions on all files in the directory
+chmod +x /home/ec2-user/server/*
+
+# If you have a specific jar file, provide its full name, like 'app.jar'
+# chmod +x /home/ec2-user/server/your-app.jar
+
 chmod +x /home/ec2-user/server/server_start.sh
 chmod +x /home/ec2-user/server/server_stop.sh
